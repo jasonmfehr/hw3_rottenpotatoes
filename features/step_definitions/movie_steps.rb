1ee3ae98757598ae6bb77003d0343d16ca901927
Jason Fehr <jasonmfehr@gmail.com>
2012-03-18 13:25:54 -0500

    completed hw3 part 2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,7 +2,7 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    Movie.create!(movie)
+    m = Movie.create!(movie)
   end
 end
 
@@ -20,7 +20,23 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(/,/).each do |rating|
+    if uncheck then
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+end
+
+Then /I should see (all|none) of the movies/ do |all_none|
+  expected = all_none.eql?("all") ? Movie.count : -1
+  page_body = page.body.gsub(/\n/, "").downcase
+  page_body =~ /<table id="movies"><thead>.*?<\/thead><tbody>(.*?)<\/tbody/
+  #puts "VAR 1: #{$1.split(/<tr>/).count - 1}"
+  #puts "ALL OR NONE: #{all_none}"
+  assert $1.split(/<tr>/).count - 1 == expected
 end
